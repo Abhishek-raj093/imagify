2fb30acbe6bcae3c153d3a5ace742b711e8e3e3b
Abhishek-raj093 <ss0986825@gmail.com>
2024-12-25 00:02:34 +0530

    Fourth Commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
+import Stripe from 'stripe';
+import transitionModel from "../models/transactionModel.js";
 
 export const registerUser = async (req, res)=>{
     try {
@@ -11,7 +13,7 @@ export const registerUser = async (req, res)=>{
         }
 
         const salt = await bcrypt.genSalt(10)
-        const handedPassword = await bcrypt(password, salt)
+        const handedPassword = await bcrypt.hash(password, salt)
 
         const userData = {
             name,
@@ -38,7 +40,7 @@ export const loginUser = async (req, res)=>{
         const user = await userModel.findOne({email})
 
         if(!user) {
-            return res({success: false, message: 'User does not exist'})
+            return res.json({success: false, message: 'User does not exist'})
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
@@ -48,7 +50,7 @@ export const loginUser = async (req, res)=>{
             res.json({success: true, token, user: {name: user.name}})
 
         }else{
-            return res({success: false, message: 'Invalid credentials'})
+            return res.json({success: false, message: 'Invalid credentials'})
         }
 
     } catch (error) {
@@ -57,3 +59,84 @@ export const loginUser = async (req, res)=>{
 
     }
 }
+const userCredits = async (req, res)=>{
+    try {
+        const {userId} = req.body
+
+        const user = await userModel.findById(userId) 
+        res.json({success: true, credits: user.creditBalance, user: {name: user.name}})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+const stripeInstance = new Stripe({
+    key_publishable: process.env.PUBLISHABLE_KEY,
+    key_secret: process.env.SECRET_KEY
+});
+
+const paymentStripe = async(req, res)=>{
+    try {
+        const {userId, planId} = req.body
+
+        const userData = await userModel.findById(userId)
+
+        if (!userId || !planId) {
+            return res.json({success: false, message: 'Missing Details'})
+        }
+
+        let credits, plan, amount, date
+
+        switch (planId) {
+            case 'Basic':
+                plan = 'Basic'
+                credits = 100
+                amount = 10
+            break;
+
+            case 'Advanced':
+                plan = 'Advanced'
+                credits = 500
+                amount = 50
+            break;
+
+            case 'Business':
+                plan = 'Business'
+                credits = 5000
+                amount = 250
+            break;
+
+            default:
+                return res.json({success: false, message: 'plan not found'});
+        }
+
+        date = Date.now();
+
+        const transactionData = {
+            userId, plan, amount, credits, date
+        }
+
+        const newTransaction = await transitionModel.create(transitionModel)
+
+        const options = {
+            amount: amount * 100,
+            currency: process.env.CURRENCY,
+            recept: newTransaction._id,
+        }
+
+        await stripeInstance.invoiceRenderingTemplates.create(SchemaTypeOptions, (error, order)=>{
+            if (error) {
+                console.log(error);
+                return res.json({success: false, message: error})
+            }
+            res.json({success: true, order})
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export {userCredits, paymentStripe}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import {registerUser} from '../controllers/userController.js'
+import {paymentStripe, registerUser, userCredits} from '../controllers/userController.js'
 import {loginUser} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
+userRouter.get('/credits', userAuth, userCredits)
+userRouter.post('/pay-stripe', userAuth, paymentStripe)
 
 export default userRouter
+
+//http://localhost:4000/api/user/register
+//http://localhost:4000/api/user/login
